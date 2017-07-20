8ffe43ab687bdd7a31b35f7e926ee7c867f639fc
ilhamarrouf <ilham.arrouf@gmail.com>
2017-07-20 11:36:17 +0700

    create lesson seeder

---
+++ database/seeds/LessonsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class LessonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 10; $i++) { 
+            DB::table('lessons')->insert([
+                'title'           => $title = $faker->sentence(),
+                'slug'            => str_replace(' ', '-', strtolower($title)),
+                'summary'         => $faker->paragraph(),
+                'url_source_code' => 'http://github.com',
+                'type'            => rand(0, 1),
+                'status'          => 1,
+                'user_id'         => rand(1, 2),
+                'published_at'    => Carbon::now('Asia/Jakarta'),
+            ]);
+        }
+    }
+}
